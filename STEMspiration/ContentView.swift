ceb246fff09e978f94757a76d4f6d6cd0cdc48f2
Yusra Saiyed <yusrasaiyed14@gmail.com>
2023-07-13 10:00:49 -0700

    stem app!

--- STEMspiration/ContentView.swift
+++ STEMspiration/ContentView.swift
@@ -9,18 +9,43 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        ZStack {
+            Color(.systemPurple)
+    .ignoresSafeArea()
+            
+        VStack(spacing: 20.0) {
+                    Image( "Mae Jamison" )
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .cornerRadius(15)
+                    
+                    
+                    HStack {
+                        
+                    }
+                    
+                    Text("Mae Jamison")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color.purple)
+                    
+                    Text ("Astronaut")
+                }
+            
+            
+            .foregroundColor(.white)
+                     
+            
         }
-        .padding()
-    }
-}
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+        
+      
+    }
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
